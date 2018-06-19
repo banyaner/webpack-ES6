097c019b3671fd3d544d6425c496fdd4393403d2
zhongjx <bjzhongjingxue@corp.netease.com>
2018-06-19 18:10:12 +0800

    修改测试的mock api

--- mock/demo.js
+++ mock/demo.js
@@ -2,9 +2,10 @@
  * Created by zhongjx on 1/20/17.
  */
 module.exports = {
-    api: '/lazyload',
+    api: '/demo',
     response: function (req, res) {
         var data = {
+            test: 334
         }
         res.send(data)
     }
