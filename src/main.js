29ab775c6656d763ce59628d3b52682b3b0290b3
zhongjx <bjzhongjingxue@corp.netease.com>
2018-07-18 15:52:09 +0800

    优化样式

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 /**
  * Created by zhongjx on 2018/6/6.
  */
-import 'normalize.css'
 import './css/common.css'
-import './css/index.css'
 
 fetch('/demo').then(res => res.json()).then((data) => {
     console.log(data)
