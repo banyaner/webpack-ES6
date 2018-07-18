a635e7064f81fbfafeaa49e1bdd7a6b71a2417c2
zhongjx <bjzhongjingxue@corp.netease.com>
2018-07-18 15:46:54 +0800

    修改px2rem黑名单

--- postcss.config.js
+++ postcss.config.js
@@ -52,7 +52,7 @@ module.exports = {
         'postcss-sprites': spriteOpts,
         'postcss-plugin-px2rem': {
             rootValue: 100,
-            propBlackList: ['border', 'border-top', 'border-bottom', 'border-left', 'border-right'],
+            propBlackList: ['border-width'],
         }
     },
 }
